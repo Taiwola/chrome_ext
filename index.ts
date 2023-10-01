4cec5795714a082380d8f4001ffa4b2c73e8e366
Taiwola <seunolanitori@gmail.com>
2023-10-01 21:17:56 +0100

    chore 3

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import helmet from "helmet";
 import router from "./router/route";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.raw({ type: "application/octet-stream" }));
